cd83117fa2f1f9541541b5c07dddae0406103c9d
JulienLamour <jlamour@bnl.gov>
2021-11-03 12:30:27 -0400

    Adding a plot of the spectra

--- R/Datasets_Map_and_resume.R
+++ R/Datasets_Map_and_resume.R
@@ -9,16 +9,17 @@ library(sf)
 library(rnaturalearth)
 library(rnaturalearthdata)
 library(rgeos)
+library(spectratrait)
 path=here()
 setwd(path)
 
-##### Importing the dataset descriptions
+### Importing the dataset descriptions
 
 ls_files=dir(recursive = TRUE)
 ls_files_Description=ls_files[which(grepl(x=ls_files,pattern="Description.csv",ignore.case = TRUE))]
 All_Description=do.call("rbind", apply(X = as.matrix(ls_files_Description),FUN = read.csv,MARGIN = 1))
 
-
+### Creating a map of all datasets
 world <- ne_countries(scale = "medium", returnclass = "sf")
 Map_datasets=ggplot(data = world) + geom_sf() + xlab("Longitude") + ylab("Latitude") +geom_point(data= All_Description,aes(x=Long, y=Lat),
                                                                                    color = "red")
@@ -28,7 +29,7 @@ dev.off()
 
 print(Map_datasets)
 
-##### Importing curated datasets
+### Importing curated datasets
 
 ls_files=dir(recursive = TRUE)
 ls_files_Curated=ls_files[which(grepl(x=ls_files,pattern="3_Spectra_traits.Rdata",ignore.case = TRUE))]
@@ -38,7 +39,7 @@ for(files in ls_files_Curated){
   data_curated=rbind.data.frame(data_curated,spectra)
 }
 
-
+### Computing the number of leaves per species
 Resume=data.frame(table(data_curated$Species))
 colnames(Resume)=c('Species','N_leaf')
 Resume$Species=as.character(Resume$Species)
@@ -48,6 +49,12 @@ p<-tableGrob(Resume)
 grid.arrange(p)
 dev.off()
 
+## Histogram of Vcmax25 in the combined dataset
 jpeg("Hist_Vcmax25.jpeg", height=100, width=100,units = 'mm',res=300)
 hist(data_curated$Vcmax25,breaks = 20,xlab=expression(italic(V)[cmax25]~mu*mol~m^-2~s^-1),ylab='Number of leaves',main='')
+dev.off()
+
+## Reflectance spectra of the combined dataset
+jpeg("Reflectance.jpeg", height=100, width=100,units = 'mm',res=300)
+f.plot.spec(Z = data_curated$Spectra,wv = 350:2500)
 dev.off()
