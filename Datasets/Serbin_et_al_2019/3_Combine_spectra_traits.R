a3fc1905b3ff92f9b73e85accfea7df79ae9c509
Shawn P. Serbin <sserbin@bnl.gov>
2022-09-19 16:57:06 -0400

    Initial add of Serbin et al. 2019; HyspIRI dataset. Lots of curation work left to do. Starting with Ag samples then will have to move on to the natural veg samples that have had area corrections applied

---
+++ Datasets/Serbin_et_al_2019/3_Combine_spectra_traits.R
@@ -0,0 +1,18 @@
+library(here)
+library(spectratrait)
+path <- here()
+setwd(paste(path,'/Datasets/Serbin_et_al_2019',sep=''))
+## !! TODO CREATE FITTED ACI DATA
+#load('2_Result_ACi_fitting.Rdata',verbose=TRUE) 
+
+
+############# Code from Shawn Serbin #################
+# UW-BNL NASA HyspIRI Airborne Campaign Leaf and Canopy Spectra and Trait Data
+ecosis_id <- "dd94f09c-1794-44f4-82e9-a7ca707a1ec0"
+spectra <- get_ecosis_data(ecosis_id = ecosis_id)
+
+# clean up
+spectra[spectra==-9999]=NA
+######################
+
+# save temp spec data
