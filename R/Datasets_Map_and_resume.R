fe32a281efee2da8338818218b0c7c92438662a2
Shawn P. Serbin <sserbin@bnl.gov>
2022-09-19 12:43:57 -0400

    Another attempt

--- R/Datasets_Map_and_resume.R
+++ R/Datasets_Map_and_resume.R
@@ -21,9 +21,10 @@ All_Description <- do.call("rbind", apply(X = as.matrix(ls_files_Description),FU
 
 ### Creating a map of all datasets
 world <- ne_countries(scale = "medium", returnclass = "sf")
-Map_datasets <- ggplot(data = world) + geom_sf() + xlab("Longitude") + ylab("Latitude") +geom_point(data= All_Description,aes(x=Long, y=Lat),
-                                                                                   color = "red")
-jpeg(filename = 'Map_datasets.jpeg',width = 170,height = 170,units = 'mm',res=150)
+Map_datasets <- ggplot(data = world) + geom_sf() + xlab("Longitude") + 
+  ylab("Latitude") +geom_point(data= All_Description,aes(x=Long, y=Lat),color = "red")
+#jpeg(filename = 'Map_datasets.jpeg',width = 170,height = 170,units = 'mm',res=150)
+png(filename = 'Map_datasets.png',width = 250,height = 180,units = 'mm',res=150)
 Map_datasets
 dev.off()
 
