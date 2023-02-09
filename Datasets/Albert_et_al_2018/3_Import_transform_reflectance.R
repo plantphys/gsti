0d0869dcb3d846fc11ab6922a7c4c4c9b09f8fa4
JulienLamour <63557789+JulienLamour@users.noreply.github.com>
2023-02-09 18:05:02 +0100

    Update pipeline

--- Datasets/Albert_et_al_2018/3_Import_transform_reflectance.R
+++ Datasets/Albert_et_al_2018/3_Import_transform_reflectance.R
@@ -14,23 +14,3 @@ f.plot.spec(Z = spectra$Reflectance,wv = 350:2500)
 spectra=spectra[,c("SampleID","Reflectance","Spectrometer","Leaf_clip")]
 save(spectra,file='3_QC_Reflectance_data.Rdata')
 
-
-spectra[spectra$Growth.Environment=="Sunlit","Sun_Shade"]="Sun"
-spectra[spectra$Growth.Environment=="Shaded","Sun_Shade"]="Shade"
-spectra=data.frame(SampleID=spectra$BR_UID,
-                        Dataset='Albert_et_al_2018',
-                        Site_name="TNF",
-                        Species=spectra$Species,
-                        N_pc=NA,
-                        Na=NA,
-                        LMA=NA,
-                        LWC=NA,
-                        Vcmax25=spectra$VcmaxRef,
-                        Jmax25=spectra$JmaxRef,
-                        Tp25=spectra$TpRef,
-                        Tleaf_ACi=spectra$Tleaf,
-                        Spectra=I(as.matrix(spectra[,11:2161]*100))) ## Reflectance in % (0 - 100)
-
-
-
-save(spectra,file='3_Spectra_traits.Rdata')
