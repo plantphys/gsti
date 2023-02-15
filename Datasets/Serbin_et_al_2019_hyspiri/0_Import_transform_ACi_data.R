79a8c50a8b401ac1deba785b019d8e020f17cb72
Shawn P. Serbin <sserbin@bnl.gov>
2023-02-15 16:32:54 -0500

    Temp update

--- Datasets/Serbin_et_al_2019_hyspiri/0_Import_transform_ACi_data.R
+++ Datasets/Serbin_et_al_2019_hyspiri/0_Import_transform_ACi_data.R
@@ -2,27 +2,34 @@ library(here)
 path <- here()
 
 source(file.path(path,'/R/Correspondance_tables_ESS.R'))
-setwd(file.path(path,'/Datasets/Serbin_et_al_2019'))
+setwd(file.path(path,'/Datasets/Serbin_et_al_2019_hyspiri'))
+getwd()
 
 src_data_dir <- file.path("LiCor_data")
-#original_data <- read.csv(file.path(src_data_dir,'CVARS_Compiled_ACi_Data_June2013.csv'), header = T)
-original_data <- read.csv(file.path(src_data_dir,'concatenated_ag_gasex_data.csv'), header = T, stringsAsFactors = F)
-head(original_data)
-names(original_data)
+
+#--------------------------------------------------------------------------------------------------#
+# curate the Ag datasets
+orig_ag_data <- read.csv(file.path(src_data_dir,'concatenated_ag_gasex_data.csv'), 
+                         header = T, stringsAsFactors = F)
+head(orig_ag_data)
+names(orig_ag_data)
+
+#!! OLD
 #curated_data <- original_data[,c("Site", "Plot","USDA_Species", "Tree_Plant_Number", "Leaf_Number", "Canopy_Position",
 #                                 "Leaf_Age", "Sample_Name", "Date", "Photo", "Ci","CO2S","CO2R","Cond","Press",
 #                                 "PARi","RH_S","Tleaf","QC","Rep")]     
+#!! OLD
 
-sampID <- paste0(original_data$Sample_Name, "_", original_data$Rep, "_", as.Date(as.character(original_data$Date), format = "%Y%m%d"))
-original_data$Sample_ID_Name <- sampID
-original_data$SampleID <- as.numeric(as.factor(sampID))
-head(original_data)
+sampID <- paste0(orig_ag_data$Sample_Name, "_", orig_ag_data$Rep, "_", 
+                 as.Date(as.character(orig_ag_data$Date), format = "%Y%m%d"))
+orig_ag_data$Sample_ID_Name <- sampID
+orig_ag_data$SampleID <- as.numeric(as.factor(sampID))
+head(orig_ag_data)
 
-curated_data <- original_data[,c("SampleID","Obs","Photo", "Ci","CO2S","CO2R","Cond","Press",
+curated_data <- orig_ag_data[,c("SampleID","Obs","Photo", "Ci","CO2S","CO2R","Cond","Press",
                                  "PARi","RH_S","Tleaf","Sample_ID_Name","QC","Rep")] 
 names(curated_data)
 head(curated_data)
-
 
 colnames(curated_data)=c(ESS_column,'Sample_ID_Name','QCauthors','Replicate')
 ##Julien modif: I changed your column name to be consistent with other datasets
@@ -52,6 +59,8 @@ head(curated_data)
 curated_data=curated_data[order(curated_data$SampleID_num),]
 
 save(curated_data,file='0_curated_data.Rdata')
+#--------------------------------------------------------------------------------------------------#
+
 
 ## TODO: NEED TO PROCESS ALL SRC DATA SIMILARLY THEN ADD TO A SINGLE L0 FILE
 
