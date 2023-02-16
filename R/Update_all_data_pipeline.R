813cb64c75f81e178082786de8b9a89a64994147
JulienLamour <63557789+JulienLamour@users.noreply.github.com>
2023-02-16 08:37:01 +0100

    Better handling of Tleaf

--- R/Update_all_data_pipeline.R
+++ R/Update_all_data_pipeline.R
@@ -7,10 +7,20 @@ check_pipeline <- TRUE
 update_database <-TRUE
 
 ## Re-run all the '2_Fit_Aci.R' codes
+
 if(refit_aci) {
-  print("**** NOT IMPLEMENTED YET ****")
+  ls_files <- dir(path = path, recursive = TRUE)
+  ls_files <- ls_files[which(grepl(x=ls_files,pattern="2_Fit_ACi.R",ignore.case = TRUE))]
+  ls_files <- file.path(path,ls_files)
+  for(i in seq_along(ls_files)){
+    print(ls_files[i])
+    source(ls_files[i])
+    Sys.sleep(1)
+  }
+} else {
+  print("**** Skipping fitting of gas exchange data **** ")
 }
-  
+
 ## Re-run all the '3_Import_transform_reflectance.R' codes - to update existing data or add new
 if(recombine_spec_trait) {
   ls_files <- dir(path = path, recursive = TRUE)
