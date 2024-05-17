97077338c8cc8ddd19861a2c3c78ecffbf930413
JulienLamour <63557789+JulienLamour@users.noreply.github.com>
2024-05-17 15:51:55 +0200

    Adding biochemistry data

--- Datasets/Lamour_et_al_2022/4_Import_transform_SampleDetails.R
+++ Datasets/Lamour_et_al_2022/4_Import_transform_SampleDetails.R
@@ -2,7 +2,8 @@ library(here)
 path=here()
 setwd(file.path(path,'/Datasets/Lamour_et_al_2022'))
 SampleDetails=read.csv("PA_2022_SampleDetails.csv")
-
+Biochemistry=read.csv("PA_2022_Foliar_biochemistry.csv")
+SampleDetails=merge(x=SampleDetails,y=Biochemistry[,c("SampleID","LMA","N_pc","fresh_mass","dry_mass")],all.x=TRUE)
 SampleDetails$Site_name=substr(x = SampleDetails$Site,start = 4,stop = 7)
 SampleDetails$Dataset_name="Lamour_et_al_2022"
 SampleDetails$Species=SampleDetails$Genus_species
@@ -15,13 +16,13 @@ SampleDetails[SampleDetails$Site_name=="Gam","Sun_Shade"]="Shade"
 SampleDetails[SampleDetails$Site_name=="Gam"&SampleDetails$Species=="Tabebuia rosea","Sun_Shade"]="Sun"
 SampleDetails$Plant_type="Wild"
 SampleDetails$Soil="Natural"
-SampleDetails$LMA=NA
-SampleDetails$Narea=NA
-SampleDetails$Nmass=NA
+SampleDetails$LMA=as.numeric(SampleDetails$LMA)
+SampleDetails$Narea=SampleDetails$N_pc/100*SampleDetails$LMA
+SampleDetails$Nmass=SampleDetails$N_pc*10
 SampleDetails$Parea=NA
 SampleDetails$Pmass=NA
-SampleDetails$LWC=NA
-
+SampleDetails$LWC=(as.numeric(SampleDetails$fresh_mass)-as.numeric(SampleDetails$dry_mass))/as.numeric(SampleDetails$fresh_mass)*100
+SampleDetails[!is.na(SampleDetails$LWC)&SampleDetails$LWC<20,"LWC"]=NA
 SampleDetails=SampleDetails[,c("SampleID","Site_name","Dataset_name","Species","Leaf_match","Sun_Shade","Phenological_stage","Plant_type","Soil","LMA","Narea","Nmass","Parea","Pmass","LWC")]
 
 save(SampleDetails,file="4_SampleDetails.Rdata")
